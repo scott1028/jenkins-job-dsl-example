665671aca0b55f8c5489875e68474fd7172c40c5
Roger Jiang <dkra4741@gmail.com>
2019-03-25 14:28:01 +0800

    fast forward commit

--- dsl/seed.groovy
+++ dsl/seed.groovy
@@ -3,3 +3,12 @@ def jobName = 'First Job'
 job(jobName) {
     description("A simple Freestyle Job")
 }
+
+pipelineJob('example') {
+    definition {
+        cps {
+            script(readFileFromWorkspace('project-a-workflow.groovy'))
+            sandbox()
+        }
+    }
+}
