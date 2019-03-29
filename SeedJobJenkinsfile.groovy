720904ac80b98be13b9007cf5bfe0f83230ecce2
Roger Jiang <dkra4741@gmail.com>
2019-03-29 16:21:26 +0800

    No meaningful message simply because self development & make development fast as possible

---
+++ SeedJobJenkinsfile.groovy
@@ -0,0 +1,54 @@
+node {
+    try {
+        stage("Build SeedJob by projects") {
+            sh """
+                ls
+            """
+            projectWithJobDsl().each { app -> createProjectJobs(app) }
+        }
+
+        stage("Build ListView") {
+            sh """
+                ls
+            """
+            jobDsl ignoreMissingFiles: true, targets: "./listView/*.groovy"
+        }
+    } catch (err) {
+        throw err
+    } finally {
+        sh """
+            ls
+        """
+
+    }
+}
+
+
+def createProjectJobs(app) {
+    stage("Build [${app.name}] Seed Job") {
+        sh """
+            echo "------Start Building [${app.name}]---------------------"
+        """
+        git branch: "${app.branch}", url: "${app.repo_url}"
+        jobDsl ignoreMissingFiles: true, targets: "./jenkins/**/seed.groovy"
+        sh """
+            ls -al
+            echo "---------------------------"
+        """
+    }
+}
+
+def projectWithJobDsl() {
+  return [
+    [
+      name: 'cra-jenkins-1',
+      repo_url: 'https://github.com/Dkra/cra-jenkins',
+      branch: 'master'
+    ],
+    [
+      name: 'cra-jenkins-2',
+      repo_url: 'https://github.com/Dkra/cra-jenkins',
+      branch: 'master'
+    ]
+  ]
+}
