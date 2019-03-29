1f0ee373b285b2c8729a66e5158fcb544857a932
Roger Jiang <dkra4741@gmail.com>
2019-03-29 16:53:35 +0800

    No meaningful message simply because self development & make development fast as possible

--- SeedJobJenkinsfile.groovy
+++ SeedJobJenkinsfile.groovy
@@ -8,8 +8,8 @@ node {
         }
 
         stage("Build ListView") {
-            sh """
-                git branch: "master", url: "https://github.com/Dkra/jenkins-job-dsl-example"
+            git branch: "master", url: "https://github.com/Dkra/jenkins-job-dsl-example"
+            sh """    
                 ls
             """
             jobDsl ignoreMissingFiles: true, targets: "./listView/*.groovy"
